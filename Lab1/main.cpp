188517678ad4b762f27026254b6e0deab543d990
Marc-Antoine Plourde <marc-antoine.plourde1@uqac.ca>
2019-10-16 01:14:44 -0400

    Fonctionnel
    Paufiner les commentaires, corriger, s'assurer que tout est beau.
    
    J'ai arranger le produit matriciel, j'avais fait une erreur et maintenant pu d'overflow. :)

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -5,6 +5,10 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,8 +18,8 @@ mutex mtx;
 
 struct Chunk
 {
-    vector<vector<vector<unsigned long int>>> chunkDeMatrices;
-    int noChunk;
+	vector<vector<vector<unsigned long int>>> chunkDeMatrices;
+	int noChunk;
 };
 
 
@@ -102,7 +106,7 @@ vector<vector<unsigned long int>> TransposerMatrice(vector<vector<unsigned long 
 * @param  &fichierDeMatrices: Le fichier texte qqui contient les matrices
 * @retval retourne le vecteur qui contient la matrice lue
 */
-vector<vector<unsigned long int>> LireMatrice(int largeurDeMatrice, int hauteurDeMatrice, int nbrIteration, ifstream &fichierDeMatrices) {
+vector<vector<unsigned long int>> LireMatrice(int largeurDeMatrice, int hauteurDeMatrice, int nbrIteration, ifstream& fichierDeMatrices) {
 
 	vector<vector<unsigned long int >> matriceTemporaire(hauteurDeMatrice, vector<unsigned long int>(largeurDeMatrice));
 
@@ -131,11 +135,10 @@ vector<vector<unsigned long int>> LireMatrice(int largeurDeMatrice, int hauteurD
 * @param  matriceB: la deixième matrice
 * @retval la matrice résultante
 */
-vector<vector<unsigned long int>> CalculerProduitScalaire(vector<vector<unsigned long int>> matriceA, vector<vector<unsigned long int>> matriceB)
+vector<vector<unsigned long int>> CalculerProduitMatriciel(vector<vector<unsigned long int>> matriceA, vector<vector<unsigned long int>> matriceB)
 {
 	int caseActuelle = 0;
-	int i = 0; //compter nombre colonnes matrice1
-	int j = 0;
+
 
 	int largeurMatriceA = matriceA.at(0).size();
 	int hauteurMatriceA = matriceA.size();
@@ -144,33 +147,24 @@ vector<vector<unsigned long int>> CalculerProduitScalaire(vector<vector<unsigned
 	int hauteurMatriceB = matriceB.size();
 
 	vector<vector<unsigned long int>>matriceC(hauteurMatriceA, vector<unsigned long int>(largeurMatriceB));
-	
+
 	int temp;
 
-	for (int k = 0; k < hauteurMatriceA; k++)//nombre ligne matrice1
+	for (int i = 0; i < hauteurMatriceA; i++)//nombre ligne matrice1
 	{
 
-		for (int l = 0; l < largeurMatriceB; l++)//le nombre de colonnes de la matrice2
+		for (int j = 0; j < largeurMatriceB; j++)//le nombre de colonnes de la matrice2
 		{
-			for (int m = 0; m < largeurMatriceA; m++)// le nombre colonnes matrice1
+			for (int k = 0; k < largeurMatriceA; k++)// le nombre colonnes matrice1
 			{
 
-				temp = (matriceA[l][m] * matriceB[m][l]);
+				temp = (matriceA[i][k] * matriceB[k][j]);
 				caseActuelle = caseActuelle + temp;
 			}
-			matriceC[j][i] = caseActuelle;
-			caseActuelle = 0;
-			if (i == (largeurMatriceB - 1))
-			{
-				i = 0;
-			}
-			else
-			{
-				i++;
-			}
-
+			matriceC[i][j] = caseActuelle;
+			
 		}
-		j++;
+		
 	}
 
 	return matriceC;
@@ -192,7 +186,7 @@ int EcrireMatricesDansFichierTexte(vector<vector<vector<unsigned long int>>> vec
 	int largeurDeMatrice = vecteurDeMatrices.at(0).at(0).size();
 	int hauteurDeMatrice = vecteurDeMatrices.at(0).size();
 
-	for (int h = 0; h<1024; h++)
+	for (int h = 0; h < vecteurDeMatrices.size(); h++)
 	{
 		for (int i = 0; i < hauteurDeMatrice; i++)
 		{
@@ -246,59 +240,113 @@ vector<vector<vector<unsigned long int>>> ChargerMatricesEnMemoireEtLesTranspose
 
 }
 
+
+/**
+* @brief  Pour traiter les fragments du vecteur de matrices sur 1, 2 ou 4 threads (selon le nombre de matrices restantes) et les rassembler dans l'ordre dans le vecteur de matrice temporaire (variable globale).
+* @note
+* @param  nbrThreads: Le nombre de threads qui traitent les matrices dans la fonction.
+* @param  noChunk: Le numéro du morceau du vecteur de matrices qui est affecté au thread qui appelé la fonction.
+* @param  vecteurProduitMatrice: Contient l'ensemble des matrices qui doivent être traiter par les threads.
+* @retval retourne 1 si la fonction s'est bien déroulée
+*/
 int CalculerChunk(int nbrThreads, int noChunk, vector<vector<vector<unsigned long int>>>vecteurProduitMatrice) {
 
-    
-
-    unsigned long int tailleChunk = vecteurTemporaire.size() / (nbrThreads * 2);
-    Chunk chunk;
-    chunk.noChunk = noChunk;
-    chunk.chunkDeMatrices = vector<vector<vector<unsigned long int>>>((vecteurProduitMatrice.size() / (nbrThreads * 2)), 
+	int tailleChunk = vecteurProduitMatrice.size() / (nbrThreads * 2);
+	Chunk chunk;
+	chunk.noChunk = noChunk;
+	chunk.chunkDeMatrices = vector<vector<vector<unsigned long int>>>((vecteurProduitMatrice.size() / (nbrThreads * 2)),
 		vector<vector<unsigned long int>>((vecteurProduitMatrice.at(0).size()), vector<unsigned long int>(vecteurProduitMatrice.at(0).at(0).size())));
 
 
-    for (int i = 0; i < tailleChunk; i++)
-    {
-        
-        chunk.chunkDeMatrices.at(i) = CalculerProduitScalaire(vecteurProduitMatrice.at(tailleChunk * noChunk), vecteurProduitMatrice.at(vecteurTemporaire.size() - (tailleChunk * noChunk) - i - 1));
+	for (int i = 0; i < tailleChunk; i++)
+	{
+		//Si l'index d'au moins une des deux matrices impliquées dans l'opération est un multiple de 100, le processus ne fait rien pendant 60 secondes avant de faire l'opération.
+		if (((((tailleChunk * noChunk) + i) > 0) && ((((tailleChunk * noChunk) + i) % 100) == 0)) || (((vecteurProduitMatrice.size() - (tailleChunk * noChunk) - i) % 100) == 0))
+		{
+			this_thread::sleep_for(std::chrono::seconds(60));//Fait dormir le thread ayant franchi la condition du if pendant 1min.
 
-    }
-    
-    lock_guard<mutex> guard(mtx);
-    int position;
-    for (int i = 0; i < tailleChunk; i++)
-    {
-        position = tailleChunk*noChunk + i;
-        vecteurTemporaire.at(position) = chunk.chunkDeMatrices.at(i);
-    }
+			chunk.chunkDeMatrices.at(i) = CalculerProduitMatriciel(vecteurProduitMatrice.at((tailleChunk * noChunk) + i),
+				vecteurProduitMatrice.at(vecteurProduitMatrice.size() - (tailleChunk * noChunk) - i - 1));
 
-    return 1;
+		}
+		else//Sinon l'opération est effectuée sans sleep.
+			chunk.chunkDeMatrices.at(i) = CalculerProduitMatriciel(vecteurProduitMatrice.at((tailleChunk * noChunk) + i),
+				vecteurProduitMatrice.at(vecteurProduitMatrice.size() - (tailleChunk * noChunk) - i - 1));
+
+	}
+
+	//Section critique
+
+	//COMMENTER UN PEU LA SECTION
+	lock_guard<mutex> guard(mtx);
+	int position;
+	for (int x = 0; x < tailleChunk; x++)
+	{
+		position = (tailleChunk * noChunk) + x;
+		vecteurTemporaire.at(position) = chunk.chunkDeMatrices.at(x);
+	}
+
+	return 1;
 }
 
 
 
 /**
-* @brief  Affecter les matrices pour le produits matriciel de la première matrice avec la dernière, la deuxième avec l'avant dernière...
+* @brief  Affectation de morceaux de vecteur de matrices aux threads afin d'effectuer le produits matriciel de la première matrice avec la dernière, la deuxième avec l'avant dernière...
 * @note
 * @param  vecteurProduitMatrice: contient toutes les matrices
 * @retval retourne un vecteur de matrices contenant 2 fois moins de matrice que le vecteur passé en paramètre.
 */
-int AffecterMatricesPourProduitMatriciel(int nbrDeThreads ,vector<vector<vector<unsigned long int>>>vecteurProduitMatrice)
+int AffecterMatricesPourProduitMatriciel(int nbrDeThreads, vector<vector<vector<unsigned long int>>>vecteurProduitMatrice)
 {
-    unsigned long int nombresDeMatrices = vecteurProduitMatrice.size();
+	unsigned long int nombresDeMatrices = vecteurProduitMatrice.size();
 
-    vecteurTemporaire = vector<vector<vector<unsigned long int>>>((nombresDeMatrices / 2), 
+	vecteurTemporaire = vector<vector<vector<unsigned long int>>>((nombresDeMatrices / 2),
 		vector<vector<unsigned long int>>((vecteurProduitMatrice.at(0).size()), vector<unsigned long int>(vecteurProduitMatrice.at(0).at(0).size())));
 
-    Chunk chunk;
+	//Switch pour l'initiation de threads en fonction du nombre de matrices restantes
+	switch (nbrDeThreads)
+	{
+		//Possible d'avoir seulement un thread qui execute CalculerChunk.
+	case 1:
+	{
+		std::thread t1([&]() {CalculerChunk(nbrDeThreads, 0, vecteurProduitMatrice); });
+		t1.join();
+		t1.~thread();//Tue les threads
+		break;
+	}
 
-    vector<thread> vecteurThreads;
+	//Impossible de traiter les opérations sur les matrices sur 4 threads, mais il y en a assez pour le faire sur 2 threads
+	case 2:
+	{
+		std::thread t1([&]() {CalculerChunk(nbrDeThreads, 0, vecteurProduitMatrice); });
+		std::thread t2([&]() {CalculerChunk(nbrDeThreads, 1, vecteurProduitMatrice); });
+		t1.join();
+		t2.join();
+		t1.~thread();
+		t2.~thread();
+		break;
+	}
 
-    for(int i = 0; i > nbrDeThreads; i++) {
+	//Il y a assez de matrices pour affectuer les produits matriciels sur les matrices en utilisant 4threads.
+	case 4:
+	{
+		std::thread t1([&]() {CalculerChunk(nbrDeThreads, 0, vecteurProduitMatrice); });
+		std::thread t2([&]() {CalculerChunk(nbrDeThreads, 1, vecteurProduitMatrice); });
+		std::thread t3([&]() {CalculerChunk(nbrDeThreads, 2, vecteurProduitMatrice); });
+		std::thread t4([&]() {CalculerChunk(nbrDeThreads, 3, vecteurProduitMatrice); });
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+		t1.~thread();
+		t2.~thread();
+		t3.~thread();
+		t4.~thread();
+		break;
+	}
 
-        vecteurThreads.emplace_back([&](){CalculerChunk(nbrDeThreads, i, vecteurProduitMatrice);});
-
-    }
+	}
 
 
 
@@ -307,11 +355,56 @@ int AffecterMatricesPourProduitMatriciel(int nbrDeThreads ,vector<vector<vector<
 
 
 /**
+* @brief  Affiche le temps d'execution ainsi que la matrice finale.
+* @note
+* @param  matriceFinale: Contient la dernière matrice résultante des opérations de produits matriciels.
+* @param  vecteurProduitMatrice: contient toutes les matrices
+* @retval Retourne 1 si la fonction s'est exécuté jusqu'à la fin.
+*/
+int AffichageFinDuProgramme(vector<vector<unsigned long int>> matriceFinale, int HeureDebutProgramme)
+{
+	int tempsExecution;
+	int compteurLigneAffichage = 0;
+
+	tempsExecution = (time(NULL) - HeureDebutProgramme);
+	cout << "\t" << "L'execution du devoir1 est termine. Le temps d'execution est de " << tempsExecution << " secondes." << "\n\n\n";
+
+	for (int x = 0; x < matriceFinale.size(); x++)
+	{
+		cout << "\t" << "Ligne" << (x + 1) << "\n\t";
+		for (int y = 0; y < matriceFinale.at(0).size(); y++)
+		{
+			string affichageCase = to_string(y) + ": " + to_string(matriceFinale[x][y]) + " ";
+			cout << setw(16) << setiosflags(ios::left) << affichageCase;
+			compteurLigneAffichage++;
+			if (compteurLigneAffichage == 5)
+			{
+				cout << "\n\t";
+				compteurLigneAffichage = 0;
+			}
+
+		}
+		cout << "\n\n";
+		compteurLigneAffichage = 0;
+	}
+
+	return 1;
+}
+
+
+
+
+/**
 * @brief  point d'entrée du programme
 * @note   les étapes du programmes sont sous forme de fonction dans le main
 * @retval retourne 0 si la fonction s'est bien déroulée
 */
 int main() {
+	/*
+		time(NULL) retourne le nombre de seconde écoulé depuis le 1er janvier 1970 à 00:00 (minuit). Cette fonction sera donc appelé à la fin du programme et la différence
+		entre la valeur de retour obtenu à la fin et celle obtenu au début permettera d'obtenir le nombre de secondes écoulé depuis le début de l'exécution.
+	*/
+	unsigned long int heureDebutDuProgramme = time(NULL);
 
 	vector<vector<vector<unsigned long int>>> vecteurDeMatricesOriginal = RemplirFichier(100, 200, 1024, 500);
 	vector<vector<vector<unsigned long int>>>vecteurDeMatricesTransposees = ChargerMatricesEnMemoireEtLesTransposer(100, 200, 1024, "mat_input.txt");
@@ -326,19 +419,36 @@ int main() {
 	// on rempli les matrices du vecteur de matrices 200 par 200 des 1024 matrices résultant du premier produit matriciel
 	for (int d = 0; d < 1024; d++)
 	{
-		vecteurProduitMatriciel[d]=CalculerProduitScalaire(vecteurDeMatricesOriginal[d], vecteurDeMatricesTransposees[d]);
+		vecteurProduitMatriciel[d] = CalculerProduitMatriciel(vecteurDeMatricesOriginal[d], vecteurDeMatricesTransposees[d]);
 
-		cout << "matrice faite" << endl;
 	}
-	
+
 	EcrireMatricesDansFichierTexte(vecteurProduitMatriciel, "mat_1024.txt");
 
-    while(vecteurProduitMatriciel.size() != 1){
+	//Étape 4b à 4c
+	while (vecteurProduitMatriciel.size() != 1) {
 
-        AffecterMatricesPourProduitMatriciel(4, vecteurProduitMatriciel);
-        vecteurProduitMatriciel = vecteurTemporaire;
+		//pour la gestion du nombre de thread en fonction du nombre de matrice
+		if (vecteurProduitMatriciel.size() >= 8)
+			AffecterMatricesPourProduitMatriciel(4, vecteurProduitMatriciel);
+		else
+			if (vecteurProduitMatriciel.size() >= 4) {
+				AffecterMatricesPourProduitMatriciel(2, vecteurProduitMatriciel);
 
-    }
+			}
+			else
+				AffecterMatricesPourProduitMatriciel(1, vecteurProduitMatriciel);
 
+		vecteurProduitMatriciel = vecteurTemporaire;
+
+
+		//concaténation de la string du nom du fichier dans lequel on écrit la matrice
+		EcrireMatricesDansFichierTexte(vecteurProduitMatriciel, ("mat_" + to_string(vecteurProduitMatriciel.size()) + ".txt"));
+	}
+
+	//Affichage message fin de programme
+	AffichageFinDuProgramme(vecteurProduitMatriciel[0], heureDebutDuProgramme);
+
+	system("pause");
 	return 0;
 }
